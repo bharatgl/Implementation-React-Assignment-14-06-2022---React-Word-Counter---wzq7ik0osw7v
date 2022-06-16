e34c6751ce516b107003b3458fbbbf97530c6b20
Bharat Goyal <bgoyal1471@gmail.com>
2022-06-16 21:26:35 +0530

    createdWordCounter

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import '../styles/App.css';
+import React from "react";
+
+import "../styles/App.css";
+import WordCounter from "./WordCounter";
 const App = () => {
-
-
   return (
     <div id="main">
+      <WordCounter />
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
